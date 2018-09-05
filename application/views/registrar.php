32e2147b9e0334d740cc97743ca585ab4b7694f1
MIGUEL ANGEL MORENO <you@example.com>
2018-09-05 12:26:52 -0500

    se creo estructura de registro

--- application/views/registrar.php
+++ application/views/registrar.php
@@ -7,6 +7,8 @@
   <link rel="stylesheet" href="<?php echo base_url(); ?>static/bootstrap/css/bootstrap-theme.css">
   <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="static/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="static/css/main.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script type="text/javascript" src="<?php echo base_url();?>static/js/dropzone.js"></script>
 
@@ -42,8 +44,61 @@
   <?php include 'static/php/navbar.php' ?>
 </head>
 <body>
-  <h1> <center>Alta de Incidencia</center></h1>
-  <form role="form" class="form" action="<?php echo base_url();?>index.php/registrar/agregar" method="post">
+<section id="form-registro">
+    <div class="container content-form">
+          <div class="row">
+              <div class="col-12 content-title">
+                    <h1 class="text-center">Alta de Incidencia</h1>
+              </div>
+          </div>
+          <div class="row">
+              <form role="form" class="form" action="<?php echo base_url();?>index.php/registrar/agregar" method="post">
+                  <div class="col-md-4 form-group">
+                      <label for="id_incidencia">
+                            <h3>Incidencias:</h3>
+                      </label>
+                    <select class="form form-control" name="id_incidencia">
+                            <?php foreach ($incidencias as $incidencia) 
+                            {
+                              echo '<option value="'.$incidencia['id_incide'].'">'.$incidencia['nombre'].'</option>';
+                            } ?>
+                    </select>
+                  </div>
+                  <div class="col-md-4 form-group">
+                        <label for="id_sucursal">
+                            <h3>Sucursales:</h3>
+                        </label>
+                    <select class="form form-control" name="id_sucursal">
+                          <?php foreach ($sucursales as $sucursal) {
+                          echo '<option value="'.$sucursal['id_sucur'].'">'.$sucursal['nombre'].'</option>';
+                          } ?>
+                    </select>
+                  </div>
+                  <div class="col-md-4 form-group">
+                      <label for="fecha_y_hora">
+                        <h3>Hora del reporte:</h3>
+                      </label>
+                      <input class="form-control" type="text" name="fecha_y_hora" id="datetimepicker" required>
+                  </div>
+               </form>
+          </div>
+          <div class="row">
+              <div class="col-md-6 form-group">
+                  <div id="zonadearchivos" class="dropzone"></div>
+              </div>
+              <div class="col-md-6 form-group">
+                  <label for="observaciones"><h3>Observaciones:</h3></label>
+                  <textarea  class="form-control" name="observaciones" rows="9" cols="80" placeholder="observaciones" required></textarea>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-12 form-group">
+                  <button class="btn btn-success btn-lg" type="submit" onclick='alert("Guardado de manera exitosa")'name="enviar" >Guardar</button>
+              </div>
+          </div>
+    </div>
+</section>
+  <!-- <form role="form" class="form" action="<?php echo base_url();?>index.php/registrar/agregar" method="post">
     <div class="container">
       <div class="row">
         <div class="col-md-12">
@@ -112,6 +167,6 @@
         </div>
       </div>
     </div>
-  </form>
+  </form> -->
 </body>
 </html>
